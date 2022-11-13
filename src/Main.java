54befaf38e3619845e395b7b0be9963df7729cab
Юлия <maltssevaulia@gmail.com>
2022-11-13 22:18:50 +0300

    первый коммит

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задача 1
+        System.out.println("Задача 1");
+        int nameOne = 987654;
+        System.out.println("Значение переменной "+ "nameOne" + " c типом unt равно "+ nameOne);
+        byte nameTwo = 77;
+        System.out.println("Значение переменной "+ "nameTwo" + " c типом byte равно "+ nameTwo);
+        short nameThree = 12345;
+        System.out.println("Значение переменной "+ "nameThree" + " c типом short равно "+ nameThree);
+        long nameFour = 998877665544332211L;
+        System.out.println("Значение переменной "+ "nameFour" + " c типом long равно "+ nameFour);
+        float nameFive = 1.56f;
+        System.out.println("Значение переменной "+ "nameFive" + " c типом float равно "+ nameFive);
+        double nameSix = 3.1415926535;
+        System.out.println("Значение переменной "+ "nameSix" + " c типом double равно "+ nameSix);
+
+        //Задача
+        System.out.println("Задача 2");
+        float firstNumber = 27.12f;
+        System.out.println(firstNumber);
+        long secondNumber987 = 987678965549L;
+        System.out.println(secondNumber987);
+        byte thirdNumber = 2;
+        System.out.println(thirdNumber);
+        short fourthNumber = 786;
+        System.out.println(fourthNumber);
+        boolean monthNumber = thirdNumber == 11;
+        System.out.println(monthNumber);
+        short fifthNumber = 569;
+        System.out.println(fifthNumber);
+        short sixthNumber = -159;
+        System.out.println(sixthNumber);
+        short seventhNumber = 27897;
+        System.out.println(seventhNumber);
+        byte eighthNumber = 67;
+        System.out.println(eighthNumber);
+
+
+
+
+
+
+
+
+    }
+}
